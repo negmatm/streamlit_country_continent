9e760855422f3f1aef6cedb8d053cbf94d2047af
negmatm <negmat@gmail.com>
2021-04-24 15:09:48 -0400

    Update CountryContinentApp.py
    Added option for capital guessing

--- CountryContinentApp.py
+++ CountryContinentApp.py
@@ -6,6 +6,7 @@ import random
 from streamlit import caching
 from streamlit.script_runner import RerunException
 from streamlit.script_request_queue import RerunData
+import requests
 
 @st.cache
 def load_countries():
@@ -22,21 +23,52 @@ random_country_continent = get_random_country_continent()
 random_country = random_country_continent[0]
 continent_of_random_country = random_country_continent[1]
 
-st.title("Which continent is " + random_country + " in?")
+game_option = st.sidebar.radio("Choose game option",("Continent", "Capital"))
+st.sidebar.title("About")
+st.sidebar.info("This app, which tests your knowledge of continents and capitals of countries, is maintained by Negmat Mullodzhanov")
 
-chosen_continent = st.radio("",("I don't know", "Africa", "Asia", "Europe", "Eurasia", "North America", "South America", "Oceania"))
+if game_option == "Continent":
 
-if st.button("Submit your answer"):
-    if chosen_continent == continent_of_random_country:
-        caching.clear_cache()
-        st.write("This is correct!")
-        st.balloons()
-    elif chosen_continent == "I don't know":
+    st.title("Which continent is " + random_country + " in?")
+
+    chosen_continent = st.radio("",("Africa", "Asia", "Europe", "Eurasia", "North America", "South America", "Oceania"))
+
+    if st.button("Submit your answer"):
+        if chosen_continent == continent_of_random_country:
+            caching.clear_cache()
+            st.write("This is correct!")
+            st.balloons()
+        else:
+            st.warning("This is not correct.  Try again!")
+
+    if st.button("I don't know"):
         st.write("The answer is: " + continent_of_random_country)
-    else:
-        st.warning("This is not correct.  Try again!")
 
-if st.button("Another question please"):
-    caching.clear_cache()
-    raise RerunException(RerunData())
-    
+    if st.button("Another question please"):
+        caching.clear_cache()
+        raise RerunException(RerunData())
+
+elif game_option == "Capital":
+    st.title("What is the capital of " + random_country + " ?")
+
+    entered_capital = st.text_input("Enter Capital")
+
+    response = requests.get(f"https://restcountries.eu/rest/v2/name/{random_country}")
+    capital_of_random_country = response.json()[0]["capital"]
+
+    if st.button("Submit your answer"):
+        if entered_capital == capital_of_random_country:
+            caching.clear_cache()
+            st.write("This is correct!")
+            st.balloons()
+        elif entered_capital == "I don't know":
+            st.write("The answer is: " + capital_of_random_country)
+        else:
+            st.warning("This is not correct.  Try again!")
+
+    if st.button("I don't know"):
+        st.write("The answer is: " + capital_of_random_country)
+
+    if st.button("Another question please"):
+        caching.clear_cache()
+        raise RerunException(RerunData())
